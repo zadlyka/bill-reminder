e0db8e8d10864e02066143d35b486d08d3004144
zadlyka <tara02zadlyka@gmail.com>
2026-05-14 14:50:13 +0700

    feat(gluestack): gluestack ui implemented

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 export default function RootLayout() {
   return <Stack />;
 }
